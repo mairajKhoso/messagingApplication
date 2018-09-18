a81cd03d8f7206363c69c5308771880d15488470
Mairaj Khoso <khoso_mairaj@yahoo.com>
2018-09-18 19:59:00 +0500

    Initial commit

---
+++ messagingApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  messagingApplication
+//
+//  Created by Mairaj khoso on 9/18/18.
+//  Copyright © 2018 Mairaj khoso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
